08ea6913e057e7410ff057d475f5a22cce949f0d
PaoloVilardi <paolo.vilardi.tonic@gmail.com>
2023-05-15 15:39:09 +0200

    map API implemented. Next on the list: adjust some css for the banner and the search-bar (they overlap sometimes) and domain search feature to be implemented

--- src/index.js
+++ src/index.js
@@ -1,13 +1,64 @@
 const myForm = document.getElementById('ip_form');
 const formInput = document.getElementById('search_bar');
-myForm.addEventListener('submit', (e) =>{
+const ipAddressValue = document.querySelector('.ip-address-value');
+const ipLocationValue = document.querySelector('.ip-location-value');
+const ipTimeZoneValue = document.querySelector('.ip-time-zone-value');
+const ipIspValue = document.querySelector('.ip-isp-value');
+
+const map = L.map('map').setView([37.40599, -122.078514], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+let marker = L.marker([37.40599, -122.078514]).addTo(map);
+
+myForm.addEventListener('submit', async (e) =>{
     e.preventDefault();
     let ipValue = ipValidation(formInput.value);
     if(ipValue){
-        // alert("IP correct!");
-        console.log(ipValue);
-        myForm.action = "https://geo.ipify.org/api/v2/country?apiKey=at_1IvFdGcCCSs7jsfZTZlbmUTMtGeXG&ipAddress=" + ipValue;
-        console.log(myForm.action);
+        myForm.action = "https://geo.ipify.org/api/v2/country,city?apiKey=at_1IvFdGcCCSs7jsfZTZlbmUTMtGeXG&ipAddress=" + ipValue;
+        let ip = "";
+        let location = "";
+        let timezone = "";
+        let isp = "";
+        let lat = "";
+        let lng = "";
+        await fetch(myForm.action, {
+            method: "GET",
+        })
+            .then(res => res.json())
+            .then(res => {              
+                ip = res.ip;            
+                location = res.location.region + ", " + res.location.country + " " + res.location.postalCode;    
+                timezone = res.location.timezone;
+                isp = res.isp;
+                lat = res.location.lat;
+                lng = res.location.lng;
+            });
+
+        /*temp*/
+        // ip = "151.60.79.68";
+        // location = "Turin, IT";
+        // timezone = "UTC-2:00";
+        // isp = "Wind Italia";
+        // lat = 51.505;
+        // lng = -0.09;
+
+        ipAddressValue.firstChild.data = ip;
+        ipLocationValue.firstChild.data = location;
+        ipTimeZoneValue.firstChild.data = timezone;
+        ipIspValue.firstChild.data = isp;
+
+        // setting up the map
+        // TODO REMOVE OTHER MARKER??
+        let zoomLevel = 13;
+        map.setView([lat, lng], zoomLevel);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        let marker = L.marker([lat, lng]).addTo(map);
+
     } else {
         alert("IP not valid!");
     }
@@ -27,8 +78,6 @@ function ipValidation(ip){
 
 function isIpValue(element){
     let elementNumber = +element;
-    console.log(typeof elementNumber);
-    console.log(elementNumber);
     return !(isNaN(elementNumber)) && elementNumber >= 0 && elementNumber <= 255; 
 }
 
